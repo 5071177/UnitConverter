a2dd1750962e6f2a670aca27aa1818c07bd82291
Yury Prokhorov <5071177@gmail.com>
2021-12-02 21:03:04 +0300

    Initial Commit

---
+++ UnitCoverter/UnitCoverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitCoverterApp.swift
+//  UnitCoverter
+//
+//  Created by Yury Prokhorov on 02.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct UnitCoverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
